00fcaf0c61aa7023467a302cea329d1c878e06ee
qc90 <qc90@foxmail.com>
2020-06-20 11:33:06 +0800

    update chain spec

--- cli/src/chain_spec.rs
+++ cli/src/chain_spec.rs
@@ -431,7 +431,7 @@ fn prochain_genesis(
 /// prochain testnet config
 pub fn prochain_testnet_config() -> ChainSpec {
     let boot_nodes = vec![
-        "/ip4/123.207.140.69/tcp/30333/p2p/QmaSMt4WafkcuYQNiTts3XfhmZymVVytfJaNfBYfjDMa2q".parse().unwrap(),
+        "/ip4/123.207.140.69/tcp/30333/p2p/12D3KooWK3veKK2VNi3oaweuSYxCsHH7oYWyPo5GTUbgSo3YNuot".parse().unwrap(),
     ];
     // let boot_nodes = vec![];
     let properties = serde_json::from_str(PRA_PROPERTIES).unwrap();
@@ -518,12 +518,12 @@ pub fn prochain_testnet_config() -> ChainSpec {
 /// prochain mainnet config
 pub fn prochain_mainnet_config() -> ChainSpec {
     let boot_nodes = vec![
-        "/ip4/123.207.140.69/tcp/30333/p2p/QmaSMt4WafkcuYQNiTts3XfhmZymVVytfJaNfBYfjDMa2q".parse().unwrap(),
+        "/ip4/123.207.140.69/tcp/30333/p2p/12D3KooWSKWQUi3CwPw4Un2CQshz8uAGxtNfnkTi47sb3r4XbaF8".parse().unwrap(),
     ];
     // let boot_nodes = vec![];
     let properties = serde_json::from_str(PRA_PROPERTIES).unwrap();
     ChainSpec::from_genesis(
-        "Prochain Mainnet",
+        "Prochain Dana",
         "Dana",
         ChainType::Live,
         || {
